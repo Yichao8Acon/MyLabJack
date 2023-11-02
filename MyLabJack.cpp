541add465bdc0d5d17d9fd431d62dac95b6c9454
Yichao8Acon <yichao.qin@aconlabs.com>
2023-11-02 16:59:16 -0700

    1.1

--- MyLabJack.cpp
+++ MyLabJack.cpp
@@ -6,52 +6,75 @@
 //macro __LINE__ to display the line number in source code that called the
 //error handler.  It also has an iteration input is useful when processing
 //results in a loop (getfirst/getnext).
-void ErrorHandler(LJ_ERROR lngErrorcode, long lngLineNumber, long lngIteration) {
+int ErrorHandler(LJ_ERROR lngErrorcode, long lngLineNumber, long lngIteration) {
     char err[255];
     if (lngErrorcode != LJE_NOERROR) {
         ErrorToString(lngErrorcode, err);
-        std::cout << "Error number = " << lngErrorcode << std::endl;
-        std::cout << "Error string = " << err << std::endl;
-        std::cout << "Source line number = " << lngLineNumber << std::endl;
-        std::cout << "Iteration = " << lngIteration << std::endl;
+//        std::cout << "Exception in MyLabJack.dll: \n";
+//        std::cout << "Error number = " << lngErrorcode;
+//        std::cout << "  " << "Error string = " << err;
+//        std::cout << "  " << "Source line number = " << lngLineNumber << std::endl;
         if (lngErrorcode > LJE_MIN_GROUP_ERROR) {
-            //Quit if this is a group error.
-            getchar();
-            exit(0);
+
+            return 1;
         }
     }
+    return 0;
+}
+
+char* LJU6::GetErrorMessage(LJ_ERROR lngErrorcode){
+
+    char err[255];
+    ErrorToString(lngErrorcode, err);
+    std::string head = "Exception in MyLabJack.dll: \r\nError number = " + std::to_string(lngErrorcode) + " Error string = ";
+    head += err;
+    char msg[1024];
+    strcpy(msg, head.c_str());
+    ULONG ulSize = strlen(msg) + sizeof(char);
+    char* pszReturn = NULL;
+
+    pszReturn = (char*)::CoTaskMemAlloc(ulSize);
+    // Copy the contents of err
+    // to the memory pointed to by pszReturn.
+    strcpy(pszReturn, msg);
+    // Return pszReturn.
+    return pszReturn;
 }
 
 int LJU6::OpenDevice() {
-    lngErrorcode = OpenLabJack(LJ_dtU6, LJ_ctUSB, "1", 1, &lngHandle); //Open the first found LabJack.
-    ErrorHandler(lngErrorcode, __LINE__, 0);
-    return lngErrorcode;
+    lngErrorcode = OpenLabJack(deviceType, LJ_ctUSB, "1", 1, &lngHandle); //Open the first found LabJack.
+    return ErrorHandler(lngErrorcode, __LINE__, 0);
 }
 
 int LJU6::Init() {
     lngErrorcode = ePut(lngHandle, LJ_ioPUT_CONFIG, LJ_chAIN_RESOLUTION, resolution, 0); //Configure resolution.
     lngErrorcode = ePut(lngHandle, LJ_ioPUT_CONFIG, LJ_chAIN_SETTLING_TIME, settlingTime, 0); //Configure settling time
     lngErrorcode = GoOne(lngHandle);
-    ErrorHandler(lngErrorcode, __LINE__, 0);
+    if (ErrorHandler(lngErrorcode, __LINE__, 0)) return lngErrorcode;
 
     for (int i = 0; i < numChannels; i++) {
         lngErrorcode = ePut(lngHandle, LJ_ioPUT_AIN_RANGE, i, LJ_rgBIP1V, 0);
-        ErrorHandler(lngErrorcode, __LINE__, 0);
+        if (ErrorHandler(lngErrorcode, __LINE__, 0)) return lngErrorcode;
     }
     lngErrorcode = GoOne(lngHandle);
+    if (ErrorHandler(lngErrorcode, __LINE__, 0)) return lngErrorcode;
 
     for (int i = 0; i < numChannels; i++) {
         lngErrorcode = AddRequest(lngHandle, LJ_ioGET_AIN, i, 0, 0, 0);
-        ErrorHandler(lngErrorcode, __LINE__, 0);
+        if (ErrorHandler(lngErrorcode, __LINE__, 0)) return lngErrorcode;
     }
     lngErrorcode = GoOne(lngHandle);
+    if (ErrorHandler(lngErrorcode, __LINE__, 0)) return lngErrorcode;
 
-    return lngErrorcode;
+    return 0;
 }
 
 double *LJU6::ReadData() {
-    GoOne(lngHandle);
-    GetFirstResult(lngHandle, &lngIOType, &lngChannel, &dblValue, 0, 0);
+    lngErrorcode = GoOne(lngHandle);
+
+    lngErrorcode = GetFirstResult(lngHandle, &lngIOType, &lngChannel, &dblValue, 0, 0);
+    if (ErrorHandler(lngErrorcode, __LINE__, 0)) return nullptr;
+
     gVoltsopen = dblValue;
     while (lngErrorcode < LJE_MIN_GROUP_ERROR) {
         switch (lngIOType) {
@@ -65,9 +88,8 @@ double *LJU6::ReadData() {
         }
         lngErrorcode = GetNextResult(lngHandle, &lngIOType, &lngChannel, &dblValue, 0, 0);
         if (lngErrorcode != LJE_NO_MORE_DATA_AVAILABLE) {
-            ErrorHandler(lngErrorcode, __LINE__, lngGetNextIteration);
+            if (ErrorHandler(lngErrorcode, __LINE__, 0)) return nullptr;
         }
-        lngGetNextIteration++;
     }
     PostProcess();
     return readings;
@@ -96,7 +118,7 @@ double *LJU6::GetAverages() {
     return averages;
 }
 
-int LJU6::GetgVoltsOpen() {
+double LJU6::GetgVoltsOpen() {
     return gVoltsopen;
 }
 
@@ -127,7 +149,7 @@ int LJU6::GetPassOrFail() {
     return min < 190 || max > 450 || diff > 81;
 }
 
-int LJU6::ReadButton(){
+int LJU6::ReadButton() {
     lngErrorcode = eDI(lngHandle, 1, &STARTBUTTON); // Read LabJack FIO1
     lngErrorcode = eDI(lngHandle, 1, &STOPBUTTON); //
     return STARTBUTTON;
@@ -140,8 +162,9 @@ __declspec(dllexport) int DAQ_OpenDevice(LJU6 *pLJU6) { return pLJU6->OpenDevice
 __declspec(dllexport) int DAQ_InitDAQ(LJU6 *pLJU6) { return pLJU6->Init(); }
 __declspec(dllexport) double *DAQ_ReadData(LJU6 *pLJU6) { return pLJU6->ReadData(); }
 __declspec(dllexport) double *DAQ_GetAverages(LJU6 *pLJU6) { return pLJU6->GetAverages(); }
-__declspec(dllexport) int DAQ_GetgVoltsOpen(LJU6 *pLJU6) { return pLJU6->GetgVoltsOpen(); }
+__declspec(dllexport) double DAQ_GetgVoltsOpen(LJU6 *pLJU6) { return pLJU6->GetgVoltsOpen(); }
 __declspec(dllexport) double *DAQ_GetVoltStats(LJU6 *pLJU6) { return pLJU6->GetVoltStats(); }
 __declspec(dllexport) int DAQ_GetPassOrFail(LJU6 *pLJU6) { return pLJU6->GetPassOrFail(); }
 __declspec(dllexport) int DAQ_ReadButton(LJU6 *pLJU6) { return pLJU6->ReadButton(); }
+__declspec(dllexport) char* DAQ_GetErrorMessage(LJU6 *pLJU6, int lngErrorcode) { return pLJU6->GetErrorMessage(lngErrorcode); }
 }
--- MyLabJack.h
+++ MyLabJack.h
@@ -4,44 +4,49 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <string>
+#include <combaseapi.h>
 #include "LabJackUD.h"
 #include "DAQ.h"
 #include "MyLabJack.h"
 
+
+//LabJack related global macros
+const long deviceType = LJ_dtU6; //LabJack U6
 LJ_ERROR lngErrorcode;
 LJ_HANDLE lngHandle = 0;
 long lngGetNextIteration;
-
 long lngIOType = 0, lngChannel = 0;
 double dblValue = 0;
-double gVoltsopen = 0;
-
-long numChannels = 14;  //Number of AIN channels, 0-16.
-long resolution = 10;    //Configure resolution of the analog inputs (pass a non-zero value for quick sampling).
+const long resolution = 10; //Configure resolution of the analog inputs (pass a non-zero value for quick sampling).
 //See section 2.6 / 3.1 for more information.
-long settlingTime = 4;  //0=5us, 1=10us, 2=100us, 3=1ms, 4=10ms
+const long settlingTime = 4;  //0=5us, 1=10us, 2=100us, 3=1ms, 4=10ms
 
+
+//Application related macros
+double gVoltsopen = 0;
+const long numChannels = 14;  //Number of AIN channels, 0-13.
 double ValueDIPort = 0;
 double ValueAIN[14] = {9999};
-
-
 double Vref = 4.09;
 double Rref = 4020;
-
-int leftChannelCounts = 7;
-
+const int leftChannelCounts = 7;
 long STARTBUTTON;
 long STOPBUTTON;
 
 class LJU6 : DAQ {
 public:
+    /**
+     * Open the first found labjack
+     * @return 0 if success, err code other wise
+     */
     int OpenDevice() override;
     int Init() override;
     double* ReadData() override;
     double* GetAverages() override;
     int GetPassOrFail() override;
     void PostProcess();
-    int GetgVoltsOpen();
+    double GetgVoltsOpen();
     int ReadButton();
 
     /**
@@ -50,11 +55,13 @@ public:
      * @return Ptr to data array. data array contains 3 stats: max, min, and diff.
      */
     double* GetVoltStats();
+    char* GetErrorMessage(LJ_ERROR lngErrorcode);
 
 private:
     double readings[14];
     double averages[3];
     double stats[3];
+    char errmsg[255];
 };
 
 #endif //MYLABJACK_MYLABJACK_H
